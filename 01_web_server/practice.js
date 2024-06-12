8374eccf3a0fabcbf33dd1d8c73243f66fdd632b
Arunvasuacchu <arunvasur@gmail.com>
2024-06-12 19:05:18 +0900

    node and bun server created

---
+++ 01_web_server/practice.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const hostname = '127.0.0.3';
+const port = 3001;
+
+
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("give a biriyani");
+    } else if(req.url === '/order') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("biriyani ordered successfully");
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("failed");
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`server is listening at http://${hostname}:${port}`)
+})
---
+++ 01_web_server/server_node.js
@@ -0,0 +1,26 @@
+const http = require('http')
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        res.statusCode = 200 // everything is good
+        res.setHeader('Content-Type', 'text/plain')
+        res.end("give me samosas");
+    } else if(req.url === '/samosa') {
+        res.statusCode = 200 // everything is good
+        res.setHeader('Content-Type', 'text/plain')
+        res.end("thanks for ordering");
+    } else {
+        res.statusCode = 404 // everything is good
+        res.setHeader('Content-Type', 'text/plain')
+        res.end("404 not found");
+    }
+})
+
+server.listen(port, hostname, () => {
+    console.log(`server is listening at http://${hostname}: ${port}`)
+})
+
