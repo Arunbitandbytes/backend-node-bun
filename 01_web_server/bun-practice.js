81f6b234f2ede3e6b0f3b92a5b9dc92829f0648f
Arunvasuacchu <arunvasur@gmail.com>
2024-06-19 19:52:58 +0900

    prettier config file issue

--- 01_web_server/bun-practice.js
+++ 01_web_server/bun-practice.js
@@ -1,18 +1,16 @@
-import {serve} from 'bun';
+import { serve } from 'bun'
 
 serve({
     fetch(request) {
-
-        const url = new URL(request.url);
-        if(request.url === '/') {
-            return new Response('hello world!', {status: 200});
-        } else if(request.url === '/python') {
-            return new Response('hello python', {status: 200});
+        const url = new URL(request.url)
+        if (request.url === '/') {
+            return new Response('hello world!', { status: 200 })
+        } else if (request.url === '/python') {
+            return new Response('hello python', { status: 200 })
         } else {
-            return new Response('error', {status: 404})
+            return new Response('error', { status: 404 })
         }
     },
     port: 3000,
-    hostname: '127.0.0.01'
-    
+    hostname: '127.0.0.01',
 })
--- 01_web_server/practice.js
+++ 01_web_server/practice.js
@@ -1,25 +1,23 @@
-const http = require('http');
+const http = require('http')
 
-const hostname = '127.0.0.3';
-const port = 3001;
-
+const hostname = '127.0.0.3'
+const port = 3001
 
 const server = http.createServer((req, res) => {
-
-    if(req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end("give a biriyani");
-    } else if(req.url === '/order') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end("biriyani ordered successfully");
+    if (req.url === '/') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('give a biriyani')
+    } else if (req.url === '/order') {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('biriyani ordered successfully')
     } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end("failed");
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('failed')
     }
-});
+})
 
 server.listen(port, hostname, () => {
     console.log(`server is listening at http://${hostname}:${port}`)
